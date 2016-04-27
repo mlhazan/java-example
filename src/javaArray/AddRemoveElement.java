442b032e7791acd327621866935f65b37c87bd6c
mlhazan <mlhazan@hotmail.com>
2016-04-26 22:26:21 -0700

    RemoveElement class changed to AddRemoveElement.

---
+++ src/javaArray/AddRemoveElement.java
@@ -0,0 +1,107 @@
+package javaArray;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Hasan This code 
+ * shows an oop way to add/remove element to array
+ * Obviously to add element we must declare a new array greater than the older 
+ * array.
+ */
+public class AddRemoveElement {
+
+     private int a[];
+
+     public AddRemoveElement(int index) {
+          a = new int[index];
+     }
+
+     public void setElement(int index, int value) {
+          a[index] = value;
+     }
+
+     public int getElement(int index) {
+          return a[index];
+     }
+}//end of class RemoveEllement
+
+class RemoveMain {
+
+     public static void main(String args[]) {
+          AddRemoveElement arr;
+          int index = 10;
+          arr = new AddRemoveElement(index);
+          Random random = new Random();
+          for (int n = 0; n < index; n++) {
+               arr.setElement(n, random.nextInt(100) + 1);
+          }
+
+          for (int n = 0; n < index; n++) {
+               System.out.print(arr.getElement(n) + "  ");
+          }
+          System.out.println();
+
+          Scanner input = new Scanner(System.in);
+          System.out.println("Search a key: ");
+          int searchKey = input.nextInt();
+          int i;
+          for (i = 0; i < index; i++) {
+               if (arr.getElement(i) == searchKey) {
+                    break;
+               }
+          }
+
+          if (i == index) {
+               System.out.println("Search key not found. Do you like to add?");
+               System.out.println("To insert the element type 1 or 2 to quit");
+               int a = input.nextInt();
+               i = index + 1;
+
+               switch (a) {
+                    case 1:
+                         AddRemoveElement newArr = new AddRemoveElement(i);
+                         for (int x = 0; x < index; ++x) {
+
+                              newArr.setElement(x, arr.getElement(x));
+                         }
+                         newArr.setElement(index, searchKey);
+                         
+
+                         for (int n = 0; n < index+1; n++) {
+                              System.out.print(newArr.getElement(n) + "  ");
+                         }
+                         System.out.println();
+
+                         break;
+
+                    default:
+                         System.exit(0);
+               }
+
+          } else {
+               System.out.println("Search key found at index " + i);
+               System.out.println("To remove the elemnt type 1 or 2 to quit");
+               int a = input.nextInt();
+               switch (a) {
+                    case 1:
+                         for (int j = i; j < index - 1; j++) {
+                              arr.setElement(j, arr.getElement(j + 1));
+
+                         }
+                         index--;
+
+                         for (int n = 0; n < index; n++) {
+                              System.out.print(arr.getElement(n) + "  ");
+                         }
+                         System.out.println();
+                         break;
+
+                    default:
+                         System.exit(0);
+               }
+          }//end of for loop
+
+     }
+
+}
---
+++ src/javaArray/BinarySearch.java
@@ -0,0 +1,101 @@
+package javaArray;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Hasan
+ */
+public class BinarySearch {
+     private int arr [];
+     private int nElems;
+     
+     public BinarySearch(int size){
+          arr = new int[size];
+          nElems = 0;
+     }
+     public int size (){
+          return nElems;
+     }
+     public int find(int searchKey){
+          int lowerBound = 0;
+          int upperBound = nElems - 1;
+          
+          int current;
+          while(true){
+               current = (lowerBound + upperBound)/2;
+               if(arr[current] == searchKey)
+                    return current; //found
+               else if (lowerBound > upperBound)
+                    return nElems;  //not Found
+               else{
+                    if (arr[current] < searchKey)
+                         lowerBound = current + 1;// searchKey is in upper half
+                    else upperBound = current - 1;// searchKey is in lower half
+               }
+          }
+     } //end of find
+     
+     public void insert (int value){
+          int j;
+          for ( j = 0; j < nElems; j++){
+               if (arr[j] > value)
+                    break;               
+          }
+          for (int k = nElems; k > j; k--){
+               arr[k] = arr[k-1];
+          }
+          arr[j] = value;
+          nElems++;
+     }
+     public boolean delete( int value){
+          int j = find(value);
+          if (j== nElems)
+               return false;
+          else{
+               for (int k=j; k < nElems; k++){
+                    arr[k] = arr[k+1];
+               }
+               nElems--;              
+          return true;
+          }
+     }
+     public void display(){
+          for (int i = 0; i < nElems; i++){
+               System.out.print(arr[i]+ " ");
+          }
+          System.out.println();
+     }
+}//end of class
+class Main{
+     public static void main(String rgs[]){
+          int maxSize = 100;
+          Random random = new Random();
+          BinarySearch arr = new BinarySearch(maxSize);
+          for (int n = 0; n < 10; n++) {
+               arr.insert(random.nextInt(100) + 1);
+          }
+          arr.display();
+          Scanner input = new Scanner(System.in);
+          System.out.println("Search a key: ");
+          int searchKey = input.nextInt();
+          
+          if(arr.find(searchKey) != arr.size()){
+               System.out.println("Found "+ searchKey);
+          }
+          else System.out.println("Not Found "+ searchKey);
+          
+          System.out.println("Max size "+ maxSize);        
+          System.out.println("Array size "+ arr.size());
+          System.out.println();
+          
+          System.out.println("Delete a key: ");
+          int a = input.nextInt();
+          arr.delete(a);
+          
+          
+          System.out.println("Max size "+ maxSize);        
+          System.out.println("Array size "+ arr.size());
+          System.out.println();
+     }
+}
