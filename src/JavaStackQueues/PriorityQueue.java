040a2c17c0bee201452d16d9d8771587c4d555ee
Hasan <Hasan@DESKTOP-8KOTL1G>
2016-06-21 04:34:03 -0400

    PriorityQueue class added

---
+++ src/JavaStackQueues/PriorityQueue.java
@@ -0,0 +1,85 @@
+package JavaStackQueues;
+
+public class PriorityQueue {
+     private final int maxSize;
+     private final long queArray[];
+     private int nItems;
+//------------------------------------------------------------------------------
+
+     public PriorityQueue(int s) {
+          maxSize = s;
+          queArray = new long[maxSize];
+          nItems = 0;
+     }
+//------------------------------------------------------------------------------
+
+     public void insert(long item) {
+          int j;
+          if (nItems == 0)
+               queArray[nItems++] = item;
+          else {
+               for (j = nItems - 1; j >=0; j--){
+                    if(item > queArray[j])
+                         queArray [j+1] = queArray [j];
+                    else 
+                         break;
+               }
+               queArray [j+1] = item;
+               nItems++;
+          }
+     }
+//------------------------------------------------------------------------------
+
+     public long remove() {
+         
+          return queArray [--nItems];
+     }
+//------------------------------------------------------------------------------
+
+     public long peekMin() {
+          return queArray[nItems-1];
+     }
+//------------------------------------------------------------------------------
+
+     public boolean isEmpty() {
+          return nItems == 0;
+     }
+//------------------------------------------------------------------------------
+
+     public boolean isFull() {
+          return nItems == maxSize;
+     }
+//------------------------------------------------------------------------------
+
+     public int size() {
+          return nItems;
+     }
+//------------------------------------------------------------------------------
+}
+//##############################################################################
+
+class PriorityQueueApp {
+
+     public static void main(String args[]) {
+          PriorityQueue theQueue = new PriorityQueue(5);
+          theQueue.insert(10);
+          theQueue.insert(20);
+          theQueue.insert(30);
+          theQueue.insert(40);
+
+          theQueue.remove();
+          theQueue.remove();
+          theQueue.remove();
+
+          theQueue.insert(50);
+          theQueue.insert(60);
+          theQueue.insert(70);
+          theQueue.insert(80);
+
+          while (!theQueue.isEmpty()) {
+               System.out.print(theQueue.peekMin()+ " ");
+               theQueue.remove();            
+          }
+          System.out.println();
+     }
+}
