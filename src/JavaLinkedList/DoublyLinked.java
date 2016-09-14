cbbb1344268634fd6898ebd7340c11d7ca9b8584
Hasan <Hasan@DESKTOP-8KOTL1G>
2016-09-14 19:50:02 -0400

    DoublyLinkedList + some Bug fixed

--- src/JavaLinkedList/DoublyLinked.java
+++ src/JavaLinkedList/DoublyLinked.java
@@ -183,9 +183,9 @@ class DoublyLinkedApp {
 
      public static void main(String[] args) { // make a new list
           DoublyLinkedList theList = new DoublyLinkedList();
-          theList.insertFirst(22); // insert at front
-          theList.insertFirst(44);
-          theList.insertFirst(66);
+//          theList.insertFirst(22); // insert at front
+//          theList.insertFirst(44);
+//          theList.insertFirst(66);
           theList.insertLast(11); // insert at rear
           theList.insertLast(33);
           theList.insertLast(55);
