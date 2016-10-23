bff126b49265e6762f1f9d19b180f83a6348161e
mlhazan <mlhazan@hotmail.com>
2016-10-23 19:16:14 -0400

    QuickSort added

--- src/JavaSorting/QuickSort.java
+++ src/JavaSorting/QuickSort.java
@@ -3,39 +3,39 @@ package JavaSorting;
 
 class QuickSort {
 
-     private long[] theArray; // ref to array theArray
+     private final int[] theArray; // ref to array theArray
      private int nElems; // number of data items
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
      public QuickSort(int max) // constructor
      {
-          theArray = new long[max]; // create the array
+          theArray = new int[max]; // create the array
           nElems = 0; // no items yet
      }
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-     public void insert(long value) // put element into array
+     public void insert(int value) // put element into array
      {
           theArray[nElems] = value; // insert it
           nElems++; // increment size
      }
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
      public void display() // displays array contents
      {
-          System.out.print("A=");
+          System.out.print("A = ");
           for (int j = 0; j < nElems; j++) // for each element,
           {
                System.out.print(theArray[j] + " "); // display it
           }
           System.out.println();
      }
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
      public void quickSort() {
           recQuickSort(0, nElems - 1);
      }
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
      public void recQuickSort(int left, int right) {
           if (right - left <= 0) // if size <= 1,
@@ -43,16 +43,16 @@ class QuickSort {
                return; // already sorted
           } else // size is 2 or larger
           {
-               long pivot = theArray[right]; // rightmost item
+               int pivot = theArray[right]; // rightmost item
 // partition range
                int partition = partitionIt(left, right, pivot);
                recQuickSort(left, partition - 1); // sort left side
                recQuickSort(partition + 1, right); // sort right side
           }
      } // end recQuickSort()
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-     public int partitionIt(int left, int right, long pivot) {
+     public int partitionIt(int left, int right, int pivot) {
           int leftPtr = left - 1; // left (after ++)
           int rightPtr = right; // right-1 (after --)
           while (true) { 
@@ -71,17 +71,17 @@ class QuickSort {
           swap(leftPtr, right); // restore pivot
           return leftPtr; // return pivot location
      } // end partitionIt()
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 
      public void swap(int dex1, int dex2) // swap two elements
      {
-          long temp = theArray[dex1]; // A into temp
+          int temp = theArray[dex1]; // A into temp
           theArray[dex1] = theArray[dex2]; // B into A
           theArray[dex2] = temp; // temp into B
      } // end swap(
-//--------------------------------------------------------------
+//------------------------------------------------------------------------------
 } // end class QuickSort
-////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 class QuickSort1App {
 
@@ -91,7 +91,7 @@ class QuickSort1App {
           arr = new QuickSort(maxSize); // create array
           for (int j = 0; j < maxSize; j++) // fill array with
           { // random numbers
-               long n = (int) (java.lang.Math.random() * 99);
+               int n = (int) (java.lang.Math.random() * 99);
                arr.insert(n);
           }
           arr.display(); // display items
